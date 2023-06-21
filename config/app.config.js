4ea9c91c30372e6bde3eadde29fca84b104b0268
fongang2019 <fongangpaul23@gmail.com>
2023-06-21 07:14:09 -0700

    BACKEND

---
+++ config/app.config.js
@@ -0,0 +1,13 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG={
+    DB:'mongodb+srv://fongangpaul22:T3fxZ3H5fXVYLhqR@react-backend.6l64vle.mongodb.net/REACT-BACKEND?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
+
+
+
